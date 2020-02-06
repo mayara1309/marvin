8b85d3641c3059444fa23d128748eefa7c901bcd
mayara1309 <59873257+mayara1309@users.noreply.github.com>
2020-02-06 10:43:53 -0300

    marvin

--- fase03/ex00/parOuImpar.js
+++ fase03/ex00/parOuImpar.js
@@ -1,7 +1,19 @@
-function parOuImpar(numero) {
-    if (numero % 2 ===0) {
-return ("impar");
-} else {
-return ("par");
+function tempo(data1, data2, unidade){
+var dataIni = new Date (data1);
+var dataFim = new Date (data2);
+switch(unidade){
+case "dia":
+var dia = Math.abs(dataIni.getTime()-
+dataFim.getTime())/1000/60/60/24;
+return Math.round(dia);
+case "mês": 
+    var mes = Math.abs(dataIni.getTime()-
+    dataFim.getTime())/1000/60/60/24/30;
+    return Math.round(mes);
+    default:
+    var periodo = Math.abs(dataIni.getTime()-
+    dataFim.getTime())/1000/60/60/24/30/12;
+    return Math.round(periodo);
 }
 }
+

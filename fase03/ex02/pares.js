27a89c6cf26cf7ce1d68f7e15d0aab8041bdc1f5
mayara1309 <59873257+mayara1309@users.noreply.github.com>
2020-02-06 09:12:40 -0300

    marvin

---
+++ fase03/ex02/pares.js
@@ -0,0 +1,9 @@
+function pares (num1, num2) {
+    var numero = num1, num2
+    var total = numero %2;
+    if ( total=== 0 ) {
+return ("Os números são pares");
+}    else {
+     return ("Os números não são pares");
+}
+}

9e78e28330766b89326f06cf12184d4a5fc60b2d
mayara1309 <59873257+mayara1309@users.noreply.github.com>
2020-02-06 09:35:58 -0300

    marvin

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,8 @@
+function vogalOuConsoante(letra)  {
+    var letra = letra.toLowerCase()
+    if (letra == 'a'|| letra == 'e'|| letra ==  'i' || letra == 'o'|| letra == 'u') {
+return "vogal";
+  } else {
+  return "consoante";
+}
+}

20bd8e936400a23ce72ae19f0350e11f08eb5658
mayara1309 <59873257+mayara1309@users.noreply.github.com>
2020-02-11 09:17:24 -0300

    MARVIN

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,16 +1,6 @@
-function ateZero(num1) {
-    var n = 0;
-    if (num1 > 0){ 
-      while(n <= num1){
-        console.log(n);
-      n++;
-      if (num1<=0){
-          while (n>=num1){
-              console.log(num1);
-              num++;
-          }
-      }
+function entreNumeros(min, max){
+    while (min<= max){
+  console.log (min, max);
+  min++;
     }
-    
-}
 }

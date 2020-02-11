db2c19297278b001deee1e5fbd7f964f4fe1610e
mayara1309 <59873257+mayara1309@users.noreply.github.com>
2020-02-11 09:10:32 -0300

    MARVIN

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,7 +1,16 @@
-function entreNumeros(min, max) {
-while (min<= max){
-    console.log (min, max);
-    min++;
-} 
+function ateZero(num1) {
+    var n = 0;
+    if (num1 > 0){ 
+      while(n <= num1){
+        console.log(n);
+      n++;
+      if (num1<=0){
+          while (n>=num1){
+              console.log(num1);
+              num++;
+          }
+      }
+    }
+    
 }
-
+}

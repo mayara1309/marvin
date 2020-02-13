2e3afa76696f88d38787cd96c801d60e0b9d1460
mayara1309 <59873257+mayara1309@users.noreply.github.com>
2020-02-13 09:09:33 -0300

    MARVIN

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,16 +1,16 @@
-function por42(num1,num2){
-    var resultado = null;
-   while(num1 <= num2){
-        if(num1 % 42 === 0){
-            return Math.max(num1 + 42)
-            
-        }
-     num1++;    
-    }
-
-        if(!resultado) {
-            return false 
-            console.log("Nao encontrado")
-        }
-}
-
+function por42 (num1,num2) {
+    var res = null;
+    var cont = 8;
+   while (num1 <= num2){
+        if (num1 % 42 === 0) {
+           cont = cont + 1;
+          }
+        if (cont===2) {
+            return (num1)
+          }
+            num1++
+          } if(!res) {
+              console.log("nao encontrado");
+            return false;
+          }
+          }

0c00a44a43cf4d03fb12ed659ea3a59971bf4ed3
mayara1309 <59873257+mayara1309@users.noreply.github.com>
2020-02-13 10:02:10 -0300

    MARVIN

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -1,15 +1,17 @@
 function fizzbuzz(num1,num2){
-    var c = num1
-    while (c <= num2){
-           if(c % 3 === 0 && c % 5 === 0){
-        console.log(c + " FizzBuzz ")
-    }else if (c % 5 === 0){
-        console.log (c + " Buzz ")
-    } else if (c % 3 === 0) {
-        console.log(c + " Fizz ")
-    } else {
-        console.log(c)
+    var n = num1
+    while(n <= num2){
+          if(n % 3 === 0 && n % 5 === 0){
+     console.log(n + " FizzBuzz ")
     }
-   c++
+     else if(n % 5 === 0){
+        console.log(n + " Buzz ")
+    }
+    else if (n % 3 === 0){
+        console.log(n + " Fizz ")
+    } else {
+           console.log(n)
+    }
+        n++
     }
 }

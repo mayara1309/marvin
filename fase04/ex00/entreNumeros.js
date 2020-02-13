c28cdc327cd905dd52b3161c4fbaed09d5fb2e40
mayara1309 <59873257+mayara1309@users.noreply.github.com>
2020-02-13 08:13:40 -0300

    MARVIN

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,16 +1,6 @@
-function por42(num1,num2){
-    var resultado = null;
-   while(num1 <= num2){
-        if(num1 % 42 === 0){
-            return Math.max(num1 + 42)
-            
-        }
-     num1++;    
-    }
-
-        if(!resultado) {
-            return false 
-            console.log("Nao encontrado")
-        }
+function entreNumeros (min,max){ 
+while (min <= max){
+    console.log (min, max);
+ min++;
 }
-
+}
